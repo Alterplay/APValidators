f39e01bccf7e5b37a869369c9684904208354767
Christopher de Haan <chrisdhaan@gmail.com>
2016-11-09 21:07:51 -0800

    Update .gitignore - Added back files types previously removed from .gitignore - Removed files from repo that had been added while the above mentioned file types were not in the .gitignore

--- Example/Pods/Target Support Files/Pods-APValidators_Example/Pods-APValidators_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_APValidators_Example : NSObject
-@end
-@implementation PodsDummy_Pods_APValidators_Example
-@end
--- Example/Pods/Target Support Files/Pods-APValidators_Tests/Pods-APValidators_Tests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_APValidators_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_APValidators_TestsVersionString[];
-
