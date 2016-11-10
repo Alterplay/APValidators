5b78603f497bb0f1503a6cf4252053969e3a50b4
Christopher de Haan <chrisdhaan@gmail.com>
2016-11-09 21:18:28 -0800

    Update .gitignore - Add back in Pods for now

---
+++ Example/Pods/Target Support Files/Pods-APValidators_Tests/Pods-APValidators_Tests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_APValidators_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_APValidators_TestsVersionString[];
+
